7502f38d14f66098b1553067f1a530aaf9ed1e5d
LucianoAAP <lucianoalmeidaap@gmail.com>
2023-11-08 20:20:49 -0300

    cria classes de testes

---
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
@@ -0,0 +1,15 @@
+package com.betrybe.museumfinder.solution;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionTypeServiceTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
