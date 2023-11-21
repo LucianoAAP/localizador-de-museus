2377e02d6f18b4d0411a7adb6cedc8b65c083537
LucianoAAP <lucianoalmeidaap@gmail.com>
2023-11-21 19:58:20 -0300

    cria classe de teste para getbyid

---
+++ src/test/java/com/betrybe/museumfinder/solution/GetMuseumByIdTest.java
@@ -0,0 +1,14 @@
+package com.betrybe.museumfinder.solution;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GetMuseumByIdTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
